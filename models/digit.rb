2cd358b1e4288ef791d05ca67e8c85f28fa62eda
Torey Hickman <torey@toreyhickman.com>
2018-08-25 11:37:49 -0400

    Add digit model

---
+++ models/digit.rb
@@ -0,0 +1,64 @@
+class Digit
+  SCANNED_INTEGER_REPRESENTATIONS = {
+    " _ " +
+    "| |" +
+    "|_|" +
+    "   " => 0,
+    
+    "   " +
+    "  |" +
+    "  |" +
+    "   " => 1,
+    
+    " _ " +
+    " _|" +
+    "|_ " +
+    "   " => 2,
+    
+    " _ " +
+    " _|" +
+    " _|" +
+    "   " => 3,
+
+    "   " +
+    "|_|" +
+    "  |" +
+    "   " => 4,
+
+    " _ " +
+    "|_ " +
+    " _|" +
+    "   " => 5,
+
+    " _ " +
+    "|_ " +
+    "|_|" +
+    "   " => 6,
+
+    " _ " +
+    "  |" +
+    "  |" +
+    "   " => 7,
+
+    " _ " +
+    "|_|" +
+    "|_|" +
+    "   " => 8,
+
+    " _ " +
+    "|_|" +
+    " _|" +
+    "   " => 9,
+  }
+  
+  def initialize(scan)
+    self.scan = scan
+  end
+  
+  def to_int
+    SCANNED_INTEGER_REPRESENTATIONS[scan]
+  end
+  
+  private
+  attr_accessor :scan
+end
---
+++ spec/models/digit_spec.rb
@@ -0,0 +1,105 @@
+require_relative "../../models/digit"
+
+RSpec.describe Digit do
+  describe "converting itself to the correct integer" do
+    it "converts to 0" do
+      scanned_zero = " _ " +
+                     "| |" +
+                     "|_|" +
+                     "   "
+      
+      digit = Digit.new(scanned_zero)
+      expect(digit.to_int).to eq 0
+    end
+    
+    it "converts to 1" do
+      scanned_one = "   " +
+                    "  |" +
+                    "  |" +
+                    "   "
+
+      digit = Digit.new(scanned_one)
+      expect(digit.to_int).to eq 1
+    end
+
+    it "converts to 2" do
+      scanned_two = " _ " +
+                    " _|" +
+                    "|_ " +
+                    "   "
+
+      digit = Digit.new(scanned_two)
+      expect(digit.to_int).to eq 2
+    end
+
+    it "converts to 3" do
+      scanned_three = " _ " +
+                      " _|" +
+                      " _|" +
+                      "   "
+
+      digit = Digit.new(scanned_three)
+      expect(digit.to_int).to eq 3
+    end
+
+    it "converts to 4" do
+      scanned_four = "   " +
+                     "|_|" +
+                     "  |" +
+                     "   "
+
+      digit = Digit.new(scanned_four)
+      expect(digit.to_int).to eq 4
+    end
+
+    it "converts to 5" do
+      scanned_five = " _ " +
+                     "|_ " +
+                     " _|" +
+                     "   "
+
+      digit = Digit.new(scanned_five)
+      expect(digit.to_int).to eq 5
+    end
+
+    it "converts to 6" do
+      scanned_six = " _ " +
+                    "|_ " +
+                    "|_|" +
+                    "   "
+
+      digit = Digit.new(scanned_six)
+      expect(digit.to_int).to eq 6
+    end
+
+    it "converts to 7" do
+      scanned_seven = " _ " +
+                      "  |" +
+                      "  |" +
+                      "   "
+
+      digit = Digit.new(scanned_seven)
+      expect(digit.to_int).to eq 7
+    end
+
+    it "converts to 8" do
+      scanned_eight = " _ " +
+                      "|_|" +
+                      "|_|" +
+                      "   "
+
+      digit = Digit.new(scanned_eight)
+      expect(digit.to_int).to eq 8
+    end
+
+    it "converts to 9" do
+      scanned_nine = " _ " +
+                     "|_|" +
+                     " _|" +
+                     "   "
+
+      digit = Digit.new(scanned_nine)
+      expect(digit.to_int).to eq 9
+    end
+  end
+end
